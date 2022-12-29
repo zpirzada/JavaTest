b34ee1f4e9985fc466f0bf584cd466f7fa4cd41b
Zeb Pirzada <zpirzada@gmail.com>
2022-12-29 22:20:52 +0100

    Changes after add statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
 
         System.out.println("Hello world! - modified in GitHub ");
         System.out.println("Changes done in the local repository");
+
+        int x = 4;
+        int y = 5;
+
+        System.out.println( "x + y = " + (x + y));
     }
 }
