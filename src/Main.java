53cfbbf04f11b1e2a1d4f94ecfea25c8312f7a76
Zeb Pirzada <zpirzada@gmail.com>
2022-12-29 22:17:25 +0100

    Changes in the local repository after downloading from Github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world! - modified in GitHub ");
+        System.out.println("Changes done in the local repository");
     }
 }
